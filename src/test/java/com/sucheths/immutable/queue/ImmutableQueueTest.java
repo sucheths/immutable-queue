a86c44d3f351328d0f668e919aec0e28b8afc94a
sucheth.shivakumar <sucheth.shivakumar@rakuten.com>
2019-07-06 18:16:45 +0900

    removed unused imports

--- src/test/java/com/sucheths/immutable/queue/ImmutableQueueTest.java
+++ src/test/java/com/sucheths/immutable/queue/ImmutableQueueTest.java
@@ -2,13 +2,11 @@ package com.sucheths.immutable.queue;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertTrue;
 
 /**
  * test class of ImmutableQueue
